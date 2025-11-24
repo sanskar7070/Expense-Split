215fd370c70896f3ccdb94cbed5ef232a4fc74c6
sanskar7070 <sanskarkarn07@gmail.com>
2025-11-24 21:46:04 +0530

    chore: ignore build and OS files

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"U5SmzNNnrsTe6GnnBSLQrMVp2elmG8MxFrMJTUbhXpQ=\"\n}"
--- .next/dev/static/chunks/app_page_tsx_28d59993._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_91f7ae1a._.js"
-],
-    source: "dynamic"
-});
